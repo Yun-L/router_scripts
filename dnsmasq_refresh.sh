b315b6feafca22e9845772dffbbc4f3815e5c5ef
root <root@localhost.localdomain>
2019-09-25 13:32:47 -0400

    pull configs from txt file and added fake address for dns spoofing

--- dnsmasq_refresh.sh
+++ dnsmasq_refresh.sh
@@ -8,15 +8,7 @@ echo "old dnsmasq.conf removed"
 service dnsmasq restart
 echo "dnsmasq restarted"
 
-echo "interface=br0
-dhcp-range=172.16.0.10,172.31.255.254,8h
-dhcp-option=3,172.16.0.1
-dhcp-option=6,172.16.0.1
-server=8.8.8.8
-log-queries
-log-dhcp
-
-# include addresses" >> dnsmasq.conf
+cat dnsmasq_configs.txt >> dnsmasq.conf
 echo "new dnsmasq.conf created"
 
 mv dnsmasq.conf /etc/
